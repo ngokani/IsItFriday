4ad725b138d1df7b0adee13b4fbf5cd59629ff95
Nikhil Gokani <nikhil.gokani@ifsworld.com>
2019-01-10 13:00:17 +0000

    Optimized

--- IsItFriday/MainActivity.cs
+++ IsItFriday/MainActivity.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
 using Android.Support.V7.App;
-using Android.Views;
 using Android.Widget;
 
 namespace IsItFriday
