f009c6aff3267556d345a390e032c4e7254aa02a
Nikhil gokani <hello@technikhil.co.uk>
2025-08-11 20:50:36 +0100

    Fix bug with countdown timer day

--- app/src/main/kotlin/uk/co/technikhil/isitfriday/ui/viewmodels/CountdownViewModel.kt
+++ app/src/main/kotlin/uk/co/technikhil/isitfriday/ui/viewmodels/CountdownViewModel.kt
@@ -1,5 +1,6 @@
 package uk.co.technikhil.isitfriday.ui.viewmodels
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -7,15 +8,25 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.DayOfWeek
+import java.time.Duration
 import java.time.LocalDateTime
 import java.time.LocalTime
 import javax.inject.Inject
 
 @HiltViewModel
 class CountdownViewModel @Inject constructor() : ViewModel() {
-    private val _countdown = mutableStateOf(TimeUntil(0, 0, 0, 0))
-    val countdown: State<TimeUntil> = _countdown
+    private val _uiState: MutableStateFlow<CountdownUiState> = MutableStateFlow(CountdownUiState())
+    val uiState: StateFlow<CountdownUiState> = _uiState.asStateFlow()
+
+    private val isItFriday
+        get() = LocalDateTime.now().dayOfWeek == DayOfWeek.FRIDAY
 
     init {
         startCountdown()
@@ -31,36 +42,42 @@ class CountdownViewModel @Inject constructor() : ViewModel() {
     private fun startCountdown() {
         val now = LocalDateTime.now()
         val target = getNextFriday()
-        val duration = java.time.Duration.between(now, target)
-        _countdown.value = TimeUntil(
-            days = duration.toDays().toInt(),
-            hours = (duration.toHours() % ONE_DAY_IN_HOURS).toInt(),
-            minutes = (duration.toMinutes() % ONE_HOUR_IN_MINUTES).toInt(),
-            seconds = ((duration.toMillis() / ONE_SECOND_IN_MILLIS) % ONE_MINUTE_IN_SECONDS).toInt()
-        )
+        val duration = Duration.between(now, target)
+        _uiState.update { state ->
+            state.copy(
+                timeUntil = TimeUntil(
+                    days = duration.toDays().toInt(),
+                    hours = (duration.toHours() % ONE_DAY_IN_HOURS).toInt(),
+                    minutes = (duration.toMinutes() % ONE_HOUR_IN_MINUTES).toInt(),
+                    seconds = ((duration.toMillis() / ONE_SECOND_IN_MILLIS) % ONE_MINUTE_IN_SECONDS).toInt()
+                ),
+                isItFriday = isItFriday
+            )
+        }
     }
 
     private fun getNextFriday(): LocalDateTime {
         val now = LocalDateTime.now()
         val currentDayOfWeek = now.dayOfWeek.value
+        val thursdayInt = DayOfWeek.THURSDAY.value
         val daysUntilFriday = when {
-            currentDayOfWeek < FRIDAY_NUMERIC_VALUE -> {
-                FRIDAY_NUMERIC_VALUE - currentDayOfWeek
+            currentDayOfWeek < thursdayInt -> {
+                thursdayInt - currentDayOfWeek
             }
 
-            currentDayOfWeek == FRIDAY_NUMERIC_VALUE -> {
+            isItFriday ||
+            currentDayOfWeek == thursdayInt -> {
                 0 // No days to add, calculate time until end of day
             }
 
             else -> {
-                DAYS_IN_WEEK - currentDayOfWeek + FRIDAY_NUMERIC_VALUE
+                DAYS_IN_WEEK - currentDayOfWeek + thursdayInt
             }
         }
         return now.plusDays(daysUntilFriday.toLong()).with(LocalTime.of(23, 59, 59))
     }
 
     private companion object {
-        const val FRIDAY_NUMERIC_VALUE = 5
         const val DAYS_IN_WEEK = 7
         const val ONE_DAY_IN_HOURS = 24
         const val ONE_HOUR_IN_MINUTES = 60
